a34ed1d9ad8fea68fa1595610f0339d2caf6e19c
PoskorkovAV <aleksandr.poskorkov@vseinstrumenti.ru>
2021-03-23 14:41:00 +0300

    add sign in component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Route, Switch } from 'react-router-dom'
+import Header from './components/header/header.component'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import AuthPage from './pages/authpage/authpage.component'
 import './App.css'
 
 function App() {
     return (
         <>
+            <Header />
             <Switch>
                 <Route exact path="/" component={HomePage} />
                 <Route path="/shop" component={ShopPage} />
+                <Route path="/auth" component={AuthPage}/>
             </Switch>
         </>
     );
